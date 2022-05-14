62e9600b91520e96abd33e726c5356c8bdc158b9
zmbelles <zmbelles@noctrl.edu>
2022-05-13 21:19:42 -0500

    full Radix implementation
    Quick & topological to come

--- Programs/Sorting Algorithms/Radix Sort/Radix.cpp
+++ Programs/Sorting Algorithms/Radix Sort/Radix.cpp
@@ -3,6 +3,80 @@
 #include <fstream>
 #include <math.h>
 using namespace std;
+int digits_in(int number) {
+   return int(log10(number) + 1);
+}
+void RadixSim(){
+    ifstream file;
+    int choice;
+    string custom_file;
+    cout << "choose which unsorted file" << endl;
+    cout << "________________________________" << endl;
+    cout << "1: 100 elements" << endl;
+    cout << "2: 1000 elements" << endl;
+    cout << "3: 10000 elements" << endl;
+    cout << "4: use my own file" << endl;
+    cout << "5: exit" << endl;
+    
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        file.open("test1.txt");
+        break;
+    case 2:
+        file.open("test2.txt");
+        break;
+    case 3:
+        file.open("test3.txt");
+    case 4:
+        cout << "Please make sure your file has been uploaded into Programs\\SortingAlgorithms before continuing" << endl;
+        cout << "What is the name of your file + its extension (i.e a text file named test would be test.txt)?" << endl;
+        cin >> custom_file;
+        file.open(custom_file);
+    }
+    RadixInt r;
+    r.populate(file);
+    r.sort();
+    r.printSorted();
+}
+void InsertionSim(){
+    ifstream file;
+    int choice;
+    string custom_file;
+    cout << "choose which unsorted file" << endl;
+    cout << "________________________________" << endl;
+    cout << "1: 100 elements" << endl;
+    cout << "2: 1000 elements" << endl;
+    cout << "3: 10000 elements" << endl;
+    cout << "4: use my own file" << endl;
+    cout << "5: exit" << endl;
+    
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        file.open("test1.txt");
+        break;
+    case 2:
+        file.open("test2.txt");
+        break;
+    case 3:
+        file.open("test3.txt");
+    case 4:
+        cout << "Please make sure your file has been uploaded into Programs\\SortingAlgorithms before continuing" << endl;
+        cout << "What is the name of your file + its extension (i.e a text file named test would be test.txt)?" << endl;
+        cin >> custom_file;
+        file.open(custom_file);
+    }
+    InsertionSort i;
+    i.populate(file);
+    i.sort();
+    i.printSorted();
+}
+void QuickSim(){
+
+}
 int find_digit(int dim, int num){
     int tmp = int(num/(pow(10,dim)));
     int sol = int(tmp%10);
@@ -10,16 +84,26 @@ int find_digit(int dim, int num){
 }
 
 void copy_vector(const vector<int>& copyFrom,vector<int>& copyTo){
+    copyTo.erase(copyTo.begin(),copyTo.end());
     for(int i=0;i<copyFrom.size();i++){
         copyTo.push_back(copyFrom[i]);
     }
 }
-void printVector(vector<int> v){
+void print_vector(vector<int> v){
         for(int i=0; i<v.size();i++){
             cout << v[i] << " ";
         }
         cout << endl;
-    }
+}
+
+class Hash{
+private:
+    int used[10007];
+public:
+
+};
+
+
 //abstract base class for all algorithms
 class IntSortingAlgs{
 protected:
@@ -43,25 +127,28 @@ public:
         cout << endl;
     }
 };
-
-/*
-pre: an open file
-post: unsorted array is populated with file contents
-assumptions: all integers, each integer is stored in its own line
-*/
 bool IntSortingAlgs::populate(istream& file){
     int tmp = 0;
     int dimTmp = 0;
+    dimensions = 0;
     size = 0;
     while(!(file.eof())){
         file >> tmp;
+        dimTmp = digits_in(tmp);
+        if(dimTmp>dimensions){
+            dimensions=dimTmp;
+        }
         unsortedArray.push_back(tmp);
         size++;
     }
 }
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
 /*
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////              ///////////////////////////////////////////////
+///////////////////////////////////  Radix Sort  ///////////////////////////////////////////////
+///////////////////////////////////      LSD     ///////////////////////////////////////////////
+///////////////////////////////////              ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 Radix Sort for use with equal length lists of integers
 Time complexity is based on the number of elements in the unsortedArray (n)
 time the number of digits in each number(d)[this would be average number if 
@@ -72,42 +159,111 @@ private:
     vector<int> bucket;
 public:
     void sort();
-    vector<int> checkThisDim(int);
+    void checkThisDim(int);
 };
 
 void RadixInt::sort(){
-    int tDim = dimensions;
+    int tDim = 0;
     //make bucket=unsortedArray
     copy_vector(unsortedArray,bucket);
-    while(tDim!=0){
+    //for each dimension
+    while(tDim<dimensions){
         checkThisDim(tDim);
-        tDim--;
+        tDim++;
     }
+    sortedArray = bucket;
 }
 
-vector<int> RadixInt::checkThisDim(int){
-    for(int i=0;i<9;i++){
-        
+void RadixInt::checkThisDim(int dim){
+    vector<int> store_dim_sort;
+    size;
+    int tmp=0;
+    //for each possible 0-9
+    for(int i=0;i<=9;i++){
+        //for all elements in the list
         for(int j=0;j<size;j++){
             //get the specific digit needed for this dimension
-            int thisNum = find_digit(i, bucket[j]);
-            cout << "thisNum: " << thisNum << endl;
+            int thisNum = find_digit(dim, bucket[j]);
+            cout << thisNum << " " << bucket[j] << endl;
             if(thisNum==i){
-                bucket.push_back(bucket[j]);
+                store_dim_sort.push_back(bucket[j]);
             }
         }
+        print_vector(store_dim_sort);
     }
-    return bucket;
+    bucket = store_dim_sort;
+    print_vector(bucket);
 }
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////                 ////////////////////////////////////////////
+/////////////////////////////////  Insertion Sort  ////////////////////////////////////////////
+/////////////////////////////////                  ////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+class InsertionSort: public IntSortingAlgs{
+public:
+    void sort();
+};
+void InsertionSort::sort(){
+    bool sorted = false;
+    bool first = true;
+    int sent;
+    int thisElement;
+    
+    for(int i=0; i<size; i++){
+        thisElement = unsortedArray[i];
+        sortedArray.push_back(thisElement);
+        sent = i;
+        while(!first && !sorted){
+            if(sortedArray[sent]<sortedArray[sent-1]){
+                swap(sortedArray[sent-1], sortedArray[sent]);
+                sent--;
+            }
+            else{
+                sorted=true;
+            }
+        }
+        sorted = false;
+        first = false;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////                  ///////////////////////////////////////////
+//////////////////////////////////    Quick Sort    ///////////////////////////////////////////
+//////////////////////////////////                  ///////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+class QuickSort : public IntSortingAlgs{
+public:
+    void sort();
+};
 
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
+
 int main(){
-    ifstream file;
-    file.open("test1.txt");
-    RadixInt r;
-    r.populate(file);
-    r.printUnsorted();
-    r.sort();
+    
+    int choice;
+    bool testing = true;
+    while(testing){
+        cout << "Sorting Algorithm Tester v. 1.01" << endl;
+        cout << "________________________________" << endl;
+        cout << "1: Radix Sort" << endl;
+        cout << "2: Insertion Sort" << endl;
+        cout << "3: Exit" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            RadixSim();
+            break;
+        case 2:
+            InsertionSim();
+            break;
+        case 3:
+            QuickSim();
+            break;
+        case 4:
+            cout << "Thank you for trying. Please consider Zachary Belles" << endl;
+            testing = false;
+            break;
+        }
+    }
     return 0;
 }
